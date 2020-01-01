e256876a872be6f6597f0d400994c9ec7cf10794
Devendra Singh <devendra.616@gmail.com>
2020-01-01 12:39:45 +0530

    modified logging only for development and pushing to heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,12 @@ import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares  = [logger];
+const middlewares  = [];
+
+//log messages only in development mode
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
