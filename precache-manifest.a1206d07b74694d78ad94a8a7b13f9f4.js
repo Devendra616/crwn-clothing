dc6ce4866de291c9a02a1b1ee7ae0a63f6b795b9
Devendra Singh <devendra.616@gmail.com>
2020-01-17 17:18:00 +0530

    Updates

---
+++ precache-manifest.a1206d07b74694d78ad94a8a7b13f9f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2838805e164e1aa80bedccfc03a8c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d756654a479e1dd1b68",
+    "url": "/static/css/main.f574e643.chunk.css"
+  },
+  {
+    "revision": "6e7ea267d72b972377e2",
+    "url": "/static/js/2.f02a4e6d.chunk.js"
+  },
+  {
+    "revision": "2ad7b80e9f86afc9d73db55a74ca30a4",
+    "url": "/static/js/2.f02a4e6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d756654a479e1dd1b68",
+    "url": "/static/js/main.e3158bb1.chunk.js"
+  },
+  {
+    "revision": "78caae2cb1504e2594c9",
+    "url": "/static/js/runtime-main.4c34bd5e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
